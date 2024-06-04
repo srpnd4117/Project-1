8f4a2c64b71eff60fd93b1bfbfdc50a107d02d5c
srpnd4117 <srpnd4117@gmail.com>
2024-06-04 22:23:11 +0530

    Scholl Details added

---
+++ School.java
@@ -0,0 +1,32 @@
+class School
+
+{
+
+	public static void main(String[]args)
+
+	{
+	
+		System.out.println("HI My School Name is Rajdhani High School");
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
